6c2d24a7d61404141f22ca7484317b15100f3541
mertdemirtas <m.demirtas99@icloud.com>
2023-05-31 13:26:13 +0300

    15.3-sum

---
+++ 15.3-sum.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = set()
+        for l in range(len(nums) - 2):
+            r = len(nums) - 1
+            mid = l + 1
+            while mid < r:
+                temp = nums[l] + nums[mid] + nums[r]
+                if temp > 0:
+                    r -= 1
+                    continue
+                if temp == 0:
+                    res.add(tuple([nums[l], nums[mid], nums[r]]))
+                mid += 1
+        return res
+
+# @lc code=end
+
